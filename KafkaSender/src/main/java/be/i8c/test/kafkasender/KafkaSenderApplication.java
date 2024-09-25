8b11b2c9f2a3095d07951d46e1196407ac835cd4
Nik De Saedeleir <de.saedeleir.nik@gmail.com>
2024-09-25 13:52:16 +0200

    vxvx

---
+++ KafkaSender/src/main/java/be/i8c/test/kafkasender/KafkaSenderApplication.java
@@ -0,0 +1,13 @@
+package be.i8c.test.kafkasender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSenderApplication.class, args);
+    }
+
+}

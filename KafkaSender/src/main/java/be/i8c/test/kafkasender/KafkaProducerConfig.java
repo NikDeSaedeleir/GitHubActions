4b9c6d904cee1aeabbdd98932d221575104691cd
Nik De Saedeleir <de.saedeleir.nik@gmail.com>
2024-09-26 14:17:36 +0200

    update

--- KafkaSender/src/main/java/be/i8c/test/kafkasender/KafkaProducerConfig.java
+++ KafkaSender/src/main/java/be/i8c/test/kafkasender/KafkaProducerConfig.java
@@ -2,6 +2,7 @@ package be.i8c.test.kafkasender;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -12,10 +13,13 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
+
+    @Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
